631234e49617f411ac2ffb52f7fda0d91e712f2f
Mahmud <mahmud.est@gmail.com>
2020-03-09 18:35:16 +0600

    commit_2

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,13 @@
+$(function() {
+  $(".clickable-link").on("mouseover", function() {
+    $(this)
+      .find(".click-suggest")
+      .addClass("click-enabled");
+  });
 
+  $(".clickable-link").on("mouseleave", function() {
+    $(this)
+      .find(".click-suggest")
+      .removeClass("click-enabled");
+  });
+});
